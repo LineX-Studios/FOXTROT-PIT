1bb0a6e176cf43570bb852decd52448b65d12ae3
unkownX <52637443+unkownX12@users.noreply.github.com>
2026-02-03 07:51:11 -0500

    SCRAPING EVENTS

--- src/main/java/com/linexstudios/foxtrot/Commands/CommandFoxtrot.java
+++ src/main/java/com/linexstudios/foxtrot/Commands/CommandFoxtrot.java
@@ -4,6 +4,7 @@ import com.linexstudios.foxtrot.Denick.DenickRunnable;
 import com.linexstudios.foxtrot.Denick.AutoDenick;
 import com.linexstudios.foxtrot.Enemy.EnemyESP;
 import com.linexstudios.foxtrot.Hud.EnemyHUD;
+import com.linexstudios.foxtrot.Hud.NickedHUD;
 import com.linexstudios.foxtrot.Hud.HUDController;
 import com.linexstudios.foxtrot.Handler.ConfigHandler;
 import net.minecraft.client.Minecraft;
@@ -29,8 +30,8 @@ public class CommandFoxtrot extends CommandBase {
     }
 
     @Override
-    public String getCommandUsage(ICommandSender sender) { 
-        return "/foxtrot <add|remove|list|alerts|toggle|clear|denick|debug|esp|autodenick|hud>"; 
+    public String getCommandUsage(ICommandSender sender) {
+        return "/foxtrot <add|remove|list|alerts|toggle|clear|denick|debug|esp|autodenick|hud|nickhud|enemyhud>";
     }
 
     @Override
@@ -40,18 +41,20 @@ public class CommandFoxtrot extends CommandBase {
     public void processCommand(ICommandSender sender, String[] args) throws CommandException {
         if (args.length == 0) {
             sender.addChatMessage(new ChatComponentText(EnumChatFormatting.GRAY + "[" + EnumChatFormatting.RED + "FOXTROT" + EnumChatFormatting.GRAY + "] " + EnumChatFormatting.RED + EnumChatFormatting.BOLD + "HELP MENU"));
-            sender.addChatMessage(new ChatComponentText("")); 
-            
+            sender.addChatMessage(new ChatComponentText(""));
+
             sender.addChatMessage(new ChatComponentText(EnumChatFormatting.YELLOW + "/foxtrot add [name] " + EnumChatFormatting.GRAY + "- Add enemy"));
             sender.addChatMessage(new ChatComponentText(EnumChatFormatting.YELLOW + "/foxtrot remove [name] " + EnumChatFormatting.GRAY + "- Remove enemy"));
             sender.addChatMessage(new ChatComponentText(EnumChatFormatting.YELLOW + "/foxtrot list " + EnumChatFormatting.GRAY + "- View enemy list"));
             sender.addChatMessage(new ChatComponentText(EnumChatFormatting.YELLOW + "/foxtrot alerts " + EnumChatFormatting.GRAY + "- Toggle join alerts"));
-            sender.addChatMessage(new ChatComponentText(EnumChatFormatting.YELLOW + "/foxtrot toggle " + EnumChatFormatting.GRAY + "- Toggle HUD"));
+            sender.addChatMessage(new ChatComponentText(EnumChatFormatting.YELLOW + "/foxtrot toggle " + EnumChatFormatting.GRAY + "- Toggle all HUDs"));
             sender.addChatMessage(new ChatComponentText(EnumChatFormatting.YELLOW + "/foxtrot esp " + EnumChatFormatting.GRAY + "- Toggle ESP"));
             sender.addChatMessage(new ChatComponentText(EnumChatFormatting.YELLOW + "/foxtrot denick [name] " + EnumChatFormatting.GRAY + "- Scrape Player"));
             sender.addChatMessage(new ChatComponentText(EnumChatFormatting.YELLOW + "/foxtrot autodenick " + EnumChatFormatting.GRAY + "- Toggle automatic denicking"));
             sender.addChatMessage(new ChatComponentText(EnumChatFormatting.YELLOW + "/foxtrot hud " + EnumChatFormatting.GRAY + "- Toggle HUD drag mode"));
-            sender.addChatMessage(new ChatComponentText(EnumChatFormatting.YELLOW + "/foxtrot clear " + EnumChatFormatting.GRAY + "- Clear list"));
+            sender.addChatMessage(new ChatComponentText(EnumChatFormatting.YELLOW + "/foxtrot nickhud " + EnumChatFormatting.GRAY + "- Toggle NickedHUD"));
+            sender.addChatMessage(new ChatComponentText(EnumChatFormatting.YELLOW + "/foxtrot enemyhud " + EnumChatFormatting.GRAY + "- Toggle EnemyHUD"));
+            sender.addChatMessage(new ChatComponentText(EnumChatFormatting.YELLOW + "/foxtrot clear " + EnumChatFormatting.GRAY + "- Clear enemy list"));
             return;
         }
 
@@ -69,7 +72,7 @@ public class CommandFoxtrot extends CommandBase {
                 if (args.length > 1) {
                     new Thread(new DenickRunnable(args[1])).start();
                 } else {
-                    sender.addChatMessage(new ChatComponentText(EnumChatFormatting.GRAY + "[" + EnumChatFormatting.RED + "PIT" + EnumChatFormatting.GRAY + "] " + EnumChatFormatting.RED + "Usage: /fx denick [name]"));
+                    sender.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Usage: /fx denick [name]"));
                 }
                 break;
 
@@ -84,8 +87,7 @@ public class CommandFoxtrot extends CommandBase {
                     String nameToAdd = args[1];
                     if (!EnemyHUD.targetList.stream().anyMatch(name -> name.equalsIgnoreCase(nameToAdd))) {
                         EnemyHUD.targetList.add(nameToAdd);
-                        sender.addChatMessage(new ChatComponentText(EnumChatFormatting.GRAY + "[" + EnumChatFormatting.RED + "PIT" + EnumChatFormatting.GRAY + "] " + 
-                            EnumChatFormatting.GREEN + "Added " + nameToAdd + " to enemies."));
+                        sender.addChatMessage(new ChatComponentText(EnumChatFormatting.GREEN + "Added " + nameToAdd + " to enemies."));
                     } else {
                         sender.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + nameToAdd + " is already on your enemy list!"));
                     }
@@ -99,11 +101,9 @@ public class CommandFoxtrot extends CommandBase {
                     String nameToRemove = args[1];
                     boolean removed = EnemyHUD.targetList.removeIf(name -> name.equalsIgnoreCase(nameToRemove));
                     if (removed) {
-                        sender.addChatMessage(new ChatComponentText(EnumChatFormatting.GRAY + "[" + EnumChatFormatting.RED + "PIT" + EnumChatFormatting.GRAY + "] " + 
-                            EnumChatFormatting.RED + "Removed " + nameToRemove + " from enemies."));
+                        sender.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Removed " + nameToRemove + " from enemies."));
                     } else {
-                        sender.addChatMessage(new ChatComponentText(EnumChatFormatting.GRAY + "[" + EnumChatFormatting.RED + "PIT" + EnumChatFormatting.GRAY + "] " + 
-                            EnumChatFormatting.YELLOW + nameToRemove + " was not found on your list."));
+                        sender.addChatMessage(new ChatComponentText(EnumChatFormatting.YELLOW + nameToRemove + " was not found on your list."));
                     }
                 } else {
                     sender.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Usage: /fx remove [name]"));
@@ -115,7 +115,7 @@ public class CommandFoxtrot extends CommandBase {
                     sendMessage(sender, EnumChatFormatting.RED + "Your list is empty.");
                 } else {
                     sender.addChatMessage(new ChatComponentText(EnumChatFormatting.AQUA + "Enemy List (" + EnemyHUD.targetList.size() + ")"));
-                    sender.addChatMessage(new ChatComponentText("")); 
+                    sender.addChatMessage(new ChatComponentText(""));
                     for (String name : EnemyHUD.targetList) {
                         sender.addChatMessage(new ChatComponentText(EnumChatFormatting.GRAY + "- " + EnemyHUD.getFormattedName(name)));
                     }
@@ -129,9 +129,9 @@ public class CommandFoxtrot extends CommandBase {
                 break;
 
             case "toggle":
-                EnemyHUD.enabled = !EnemyHUD.enabled;
+                HUDController.setEnabled(!HUDController.enabled);
                 ConfigHandler.saveConfig();
-                sendMessage(sender, EnumChatFormatting.YELLOW + "Toggle HUD: " + (EnemyHUD.enabled ? EnumChatFormatting.GREEN + "ON" : EnumChatFormatting.RED + "OFF"));
+                sendMessage(sender, EnumChatFormatting.YELLOW + "HUDs: " + (HUDController.enabled ? EnumChatFormatting.GREEN + "ON" : EnumChatFormatting.RED + "OFF"));
                 break;
 
             case "clear":
@@ -143,13 +143,24 @@ public class CommandFoxtrot extends CommandBase {
             case "autodenick":
                 AutoDenick.enabled = !AutoDenick.enabled;
                 ConfigHandler.saveConfig();
-                sendMessage(sender, EnumChatFormatting.YELLOW + "AutoDenick: " + 
-                    (AutoDenick.enabled ? EnumChatFormatting.GREEN + "ON" : EnumChatFormatting.RED + "OFF"));
+                sendMessage(sender, EnumChatFormatting.YELLOW + "AutoDenick: " + (AutoDenick.enabled ? EnumChatFormatting.GREEN + "ON" : EnumChatFormatting.RED + "OFF"));
                 break;
 
             case "hud":
                 HUDController.toggleDragMode();
                 sendMessage(sender, EnumChatFormatting.YELLOW + "HUD Drag Mode: " + (HUDController.dragMode ? EnumChatFormatting.GREEN + "ON" : EnumChatFormatting.RED + "OFF"));
+                break;
+
+            case "nickhud":
+                NickedHUD.enabled = !NickedHUD.enabled;
+                ConfigHandler.saveConfig();
+                sendMessage(sender, EnumChatFormatting.YELLOW + "NickedHUD: " + (NickedHUD.enabled ? EnumChatFormatting.GREEN + "ON" : EnumChatFormatting.RED + "OFF"));
+                break;
+
+            case "enemyhud":
+                EnemyHUD.enabled = !EnemyHUD.enabled;
+                ConfigHandler.saveConfig();
+                sendMessage(sender, EnumChatFormatting.YELLOW + "EnemyHUD: " + (EnemyHUD.enabled ? EnumChatFormatting.GREEN + "ON" : EnumChatFormatting.RED + "OFF"));
                 break;
 
             default:
@@ -160,15 +171,8 @@ public class CommandFoxtrot extends CommandBase {
 
     @Override
     public List<String> addTabCompletionOptions(ICommandSender sender, String[] args, BlockPos pos) {
-        if (args.length == 1) return getListOfStringsMatchingLastWord(args, "add", "remove", "list", "alerts", "toggle", "clear", "denick", "debug", "esp", "autodenick", "hud");
-        if (args.length == 2 && (args[0].equalsIgnoreCase("add") || args[0].equalsIgnoreCase("denick"))) {
-            return getListOfStringsMatchingLastWord(args, Minecraft.getMinecraft().getNetHandler().getPlayerInfoMap().stream().map(info -> info.getGameProfile().getName()).collect(Collectors.toList()));
+        if (args.length == 1) {
+            return getListOfStringsMatchingLastWord(args,
+                    "add", "remove", "list", "alerts", "toggle", "clear", "denick", "debug", "esp", "autodenick", "hud", "nickhud", "enemyhud");
         }
-        if (args.length == 2 && args[0].equalsIgnoreCase("remove")) {
-            return getListOfStringsMatchingLastWord(args, EnemyHUD.targetList);
-        }
-        return null;
-    }
-
-    private void sendMessage(ICommandSender sender, String msg) {
-        sender.addChat
+        if (args.length == 2 && (args[0].equalsIgnoreCase("add") || args
--- src/main/java/com/linexstudios/foxtrot/Hud/HUDController.java
+++ src/main/java/com/linexstudios/foxtrot/Hud/HUDController.java
@@ -8,20 +8,17 @@ public class HUDController {
     public static boolean enabled = true;
 
     private static final NickedHUD nickedHUD = new NickedHUD();
-    private static final EventsHUD eventsHUD = new EventsHUD();
     private static final EnemyHUD enemyHUD = new EnemyHUD();
 
     public static void toggleDragMode() {
         dragMode = !dragMode;
         NickedHUD.dragMode = dragMode;
-        EventsHUD.dragMode = dragMode;
         EnemyHUD.dragMode = dragMode;
     }
 
     public static void setEnabled(boolean state) {
         enabled = state;
         NickedHUD.enabled = state;
-        EventsHUD.enabled = state;
         EnemyHUD.enabled = state;
     }
 
@@ -29,7 +26,6 @@ public class HUDController {
     public void onRender(RenderGameOverlayEvent.Post event) {
         if (!enabled) return;
         if (NickedHUD.enabled) nickedHUD.onRender(event);
-        if (EventsHUD.enabled) eventsHUD.onRender(event);
         if (EnemyHUD.enabled) enemyHUD.onRender(event);
     }
 }
