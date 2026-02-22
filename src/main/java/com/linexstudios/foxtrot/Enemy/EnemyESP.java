e62cc6aee73fc892403dabb80aed015452bba2e4
unkownX <52637443+unkownX12@users.noreply.github.com>
2026-02-22 18:11:38 -0500

    Update EnemyESP.java

--- src/main/java/com/linexstudios/foxtrot/Enemy/EnemyESP.java
+++ src/main/java/com/linexstudios/foxtrot/Enemy/EnemyESP.java
@@ -3,6 +3,7 @@ package com.linexstudios.foxtrot.Enemy;
 import com.linexstudios.foxtrot.Hud.EnemyHUD;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderGlobal;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
@@ -22,12 +23,7 @@ public class EnemyESP {
 
         for (EntityPlayer player : mc.theWorld.playerEntities) {
             if (player != mc.thePlayer && isTarget(player.getName())) {
-                // 1. Render the translucent red fill
-                renderFilledBox(player, event.partialTicks);
-                // 2. Render the solid red outline
-                renderOutlineBox(player, event.partialTicks);
-                // 3. Render the white Skeleton Highlighter (NEW)
-                renderSkeleton(player, event.partialTicks);
+                renderESP(player, event.partialTicks);
             }
         }
     }
@@ -36,35 +32,40 @@ public class EnemyESP {
         return EnemyHUD.targetList.stream().anyMatch(target -> target.equalsIgnoreCase(name));
     }
 
-    private void renderFilledBox(EntityPlayer player, float partialTicks) {
+    private void renderESP(EntityPlayer player, float partialTicks) {
         AxisAlignedBB bb = getInterpolatedBB(player, partialTicks);
-        GL11.glPushMatrix();
-        GL11.glPushAttrib(GL11.GL_ALL_ATTRIB_BITS);
-        GL11.glEnable(GL11.GL_BLEND);
-        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-        GL11.glDisable(GL11.GL_TEXTURE_2D);
-        GL11.glDisable(GL11.GL_DEPTH_TEST);
-        GL11.glDepthMask(false);
 
+        // --- PREPARE GL STATE (Wallhack + Transparency) ---
+        GlStateManager.pushMatrix();
+        GlStateManager.enableBlend();
+        GlStateManager.disableDepth();
+        GlStateManager.disableTexture2D();
+        GlStateManager.disableLighting();
+        GlStateManager.depthMask(false);
+        GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+
+        // 1. Render the translucent red fill
         drawFilledBoundingBox(bb, 1.0F, 0.3F, 0.3F, 0.4F);
 
-        GL11.glPopAttrib();
-        GL11.glPopMatrix();
-    }
-
-    private void renderOutlineBox(EntityPlayer player, float partialTicks) {
-        AxisAlignedBB bb = getInterpolatedBB(player, partialTicks);
-        GL11.glPushMatrix();
-        GL11.glPushAttrib(GL11.GL_ALL_ATTRIB_BITS);
-        GL11.glDisable(GL11.GL_TEXTURE_2D);
-        GL11.glDisable(GL11.GL_DEPTH_TEST);
-        GL11.glDepthMask(false);
-
+        // 2. Render the solid red outline (Uses Minecraft's native bounding box renderer)
+        GL11.glLineWidth(2.0F);
         GlStateManager.color(1.0F, 0.3F, 0.3F, 1.0F);
-        drawBoundingBoxOutline(bb);
+        RenderGlobal.drawOutlinedBoundingBox(bb, 255, 76, 76, 255);
 
-        GL11.glPopAttrib();
-        GL11.glPopMatrix();
+        // 3. Render the white Skeleton Highlighter
+        renderSkeleton(player, partialTicks);
+
+        // --- RESTORE GL STATE EXACTLY AS MINECRAFT EXPECTS IT ---
+        GlStateManager.depthMask(true);
+        GlStateManager.enableLighting();
+        GlStateManager.enableTexture2D();
+        GlStateManager.enableDepth();
+        GlStateManager.disableBlend();
+        
+        // CRITICAL FIX: Reset color back to pure white so it doesn't ghost/leak
+        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+        GL11.glLineWidth(1.0F);
+        GlStateManager.popMatrix();
     }
 
     private void renderSkeleton(EntityPlayer player, float partialTicks) {
@@ -73,14 +74,9 @@ public class EnemyESP {
         double y = player.lastTickPosY + (player.posY - player.lastTickPosY) * partialTicks - mc.getRenderManager().viewerPosY;
         double z = player.lastTickPosZ + (player.posZ - player.lastTickPosZ) * partialTicks - mc.getRenderManager().viewerPosZ;
 
-        GL11.glPushMatrix();
-        GL11.glPushAttrib(GL11.GL_ALL_ATTRIB_BITS);
-        GL11.glDisable(GL11.GL_TEXTURE_2D);
-        GL11.glDisable(GL11.GL_DEPTH_TEST);
-        GL11.glDepthMask(false);
         GL11.glEnable(GL11.GL_LINE_SMOOTH);
         GL11.glLineWidth(2.5F); // Thicker line for better visibility far away
-        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F); // Solid White
+        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F); // Solid White
 
         GL11.glBegin(GL11.GL_LINES);
         
@@ -105,8 +101,7 @@ public class EnemyESP {
         GL11.glVertex3d(x + 0.2, y + 0.7, z); GL11.glVertex3d(x + 0.2, y + 0.1, z);
 
         GL11.glEnd();
-        GL11.glPopAttrib();
-        GL11.glPopMatrix();
+        GL11.glDisable(GL11.GL_LINE_SMOOTH);
     }
 
     private AxisAlignedBB getInterpolatedBB(EntityPlayer player, float partialTicks) {
@@ -159,39 +154,6 @@ public class EnemyESP {
         worldrenderer.pos(bb.maxX, bb.maxY, bb.minZ).endVertex();
         worldrenderer.pos(bb.maxX, bb.maxY, bb.maxZ).endVertex();
         worldrenderer.pos(bb.maxX, bb.minY, bb.maxZ).endVertex();
-
-        tessellator.draw();
-    }
-
-    public static void drawBoundingBoxOutline(AxisAlignedBB bb) {
-        Tessellator tessellator = Tessellator.getInstance();
-        WorldRenderer worldrenderer = tessellator.getWorldRenderer();
-        worldrenderer.begin(GL11.GL_LINES, DefaultVertexFormats.POSITION);
-
-        worldrenderer.pos(bb.minX, bb.minY, bb.minZ).endVertex();
-        worldrenderer.pos(bb.maxX, bb.minY, bb.minZ).endVertex();
-        worldrenderer.pos(bb.maxX, bb.minY, bb.minZ).endVertex();
-        worldrenderer.pos(bb.maxX, bb.minY, bb.maxZ).endVertex();
-        worldrenderer.pos(bb.maxX, bb.minY, bb.maxZ).endVertex();
-        worldrenderer.pos(bb.minX, bb.minY, bb.maxZ).endVertex();
-        worldrenderer.pos(bb.minX, bb.minY, bb.maxZ).endVertex();
-        worldrenderer.pos(bb.minX, bb.minY, bb.minZ).endVertex();
-        worldrenderer.pos(bb.minX, bb.maxY, bb.minZ).endVertex();
-        worldrenderer.pos(bb.maxX, bb.maxY, bb.minZ).endVertex();
-        worldrenderer.pos(bb.maxX, bb.maxY, bb.minZ).endVertex();
-        worldrenderer.pos(bb.maxX, bb.maxY, bb.maxZ).endVertex();
-        worldrenderer.pos(bb.maxX, bb.maxY, bb.maxZ).endVertex();
-        worldrenderer.pos(bb.minX, bb.maxY, bb.maxZ).endVertex();
-        worldrenderer.pos(bb.minX, bb.maxY, bb.maxZ).endVertex();
-        worldrenderer.pos(bb.minX, bb.maxY, bb.minZ).endVertex();
-        worldrenderer.pos(bb.minX, bb.minY, bb.minZ).endVertex();
-        worldrenderer.pos(bb.minX, bb.maxY, bb.minZ).endVertex();
-        worldrenderer.pos(bb.maxX, bb.minY, bb.minZ).endVertex();
-        worldrenderer.pos(bb.maxX, bb.maxY, bb.minZ).endVertex();
-        worldrenderer.pos(bb.maxX, bb.minY, bb.maxZ).endVertex();
-        worldrenderer.pos(bb.maxX, bb.maxY, bb.maxZ).endVertex();
-        worldrenderer.pos(bb.minX, bb.minY, bb.maxZ).endVertex();
-        worldrenderer.pos(bb.minX, bb.maxY, bb.maxZ).endVertex();
 
         tessellator.draw();
     }
